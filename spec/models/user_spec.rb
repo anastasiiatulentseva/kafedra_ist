061675eb718f80e801f2b10d3b067c40a2b9d7cf
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2016-07-22 20:45:08 +0300

    configure devise and user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  it "is valid with name, email, password" do
+    user = User.new(name: "Student", email: "ex@email.com", password: "password")
+    expect(user).to be_valid
+  end
+
+  it { is_expected.to validate_presence_of(:name) }
+
+end
