6969dd1574ba3eccede27ab554174d01e532b52c
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2016-07-27 12:20:25 +0300

    fix guest user creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,7 +43,9 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    u = User.create(name: "Guest", email: "guest_#{Time.now.to_i}#{rand(100)}@example.com")
+    u = User.create(name: "Guest",
+                    email: "guest_#{Time.now.to_i}#{rand(100)}@example.com",
+                    guest: true)
     u.save!(:validate => false)
     session[:guest_user_id] = u.id
     u
