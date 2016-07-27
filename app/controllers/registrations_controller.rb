90441f3593463755b53c95c40a945c64a168795c
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2016-07-27 12:21:45 +0300

    fix authentication in controllers, minor fixes

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,13 +1,23 @@
 class RegistrationsController < Devise::RegistrationsController
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def update_resource(recource, params)
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    if current_user.admin?
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture, roles: [] ])
+    else
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture ])
+    end
+  end
+
+  def update_resource(resource, params)
     resource.update_without_password(params)
   end
 
   def after_update_path_for(resource)
     user_path(resource)
   end
+
 end
