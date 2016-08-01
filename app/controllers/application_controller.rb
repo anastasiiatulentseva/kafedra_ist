262a20cb578f875f71a06afbb7b3deda0a09e717
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2016-08-01 16:44:35 +0300

    add guest builder

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,12 +43,10 @@ class ApplicationController < ActionController::Base
   end
 
   def create_guest_user
-    u = User.create(name: "Guest",
-                    email: "guest_#{Time.now.to_i}#{rand(100)}@example.com",
-                    guest: true)
-    u.save!(:validate => false)
-    session[:guest_user_id] = u.id
-    u
+    guest = UserBuilders::Guest.new.build
+    guest.save(validate: false)
+    session[:guest_user_id] = guest.id
+    guest
   end
 
 end
