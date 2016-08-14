117244d158676cf1359c1ccbbd8fb98ff75d6df5
Anastasiia Tulentseva <nastena-skid@yandex.ru>
2016-08-14 19:38:00 +0300

    add group and specialty fields to sign up and edit profile forms for students

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,15 +2,25 @@ class RegistrationsController < Devise::RegistrationsController
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def new
+    @specialties = Specialty.all
+    super
+  end
+
+  def edit
+    @specialties = Specialty.all
+    super
+  end
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :group, :specialty_id])
 
     if current_or_guest_user.admin?
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture, roles: [] ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture, :group, :specialty_id, roles: [] ])
     else
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture ])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :picture, :group, :specialty_id])
     end
   end
 
